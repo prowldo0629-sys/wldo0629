56b4557ca530f376e98c759093e9c75badaf9d56
prowldo0629-sys <prowldo0629@gmail.com>
2026-03-26 16:17:32 +0900

    ch06 2026.03.26

---
+++ ch06/forloop.js
@@ -0,0 +1,28 @@
+// console.log("helllo world")
+// console.log("helllo world")
+// console.log("helllo world")
+
+// for(초기문; 조건문; 증감문) { }
+// i가 5와 같거나 커질때 까지 i를 1씩 늘리며 반복하겠다
+//for(let i == 0; i < 5; i++){
+
+//   for(let i = 0; i < 10; i++){
+//   console.log("Hello Wotld");
+
+// }
+
+
+//  let dan = 7;
+//    for (let i = 1; i <= 9; i++) {
+//    console.log(`${dan} x ${i} = ${dan * i}`);
+//    }
+
+// 1~10까지 총합을 for문을 사용해 구하기
+let sum = 0
+for(let i = 1; i < 11; i++) {
+
+    console.log(`${sum} + ${i}`)
+    sum += i
+}
+
+console.log(sum)
